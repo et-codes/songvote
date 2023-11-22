c7bc213b575aa3c56586cb113fb111f7590d385a
et-codes <ericthornton43@gmail.com>
2023-11-22 22:57:43 +0000

    Listen and serve

--- cmd/songvote/main.go
+++ cmd/songvote/main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+
+	"github.com/et-codes/songvote"
+)
+
+const (
+	port = ":5050"
+)
 
 func main() {
-	fmt.Println("Groovy")
+	handler := http.HandlerFunc(songvote.Server)
+	log.Fatal(http.ListenAndServe(port, handler))
 }
