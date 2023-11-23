e2a084182d8a562a43375c500325de4d4db9bbde
et-codes <ericthornton43@gmail.com>
2023-11-23 22:02:00 +0000

    Rename test

--- song_store_test.go
+++ song_store_test.go
@@ -6,24 +6,20 @@ import (
 	"github.com/et-codes/songvote"
 )
 
-func TestInMemorySongStore(t *testing.T) {
+func TestSongStore(t *testing.T) {
 	store := songvote.InMemorySongStore{}
+	newSong := songvote.Song{
+		Name:   "Creep",
+		Artist: "Radiohead",
+	}
 
 	t.Run("can add and retreive a song", func(t *testing.T) {
-		newSong := songvote.Song{
-			Name:   "Creep",
-			Artist: "Radiohead",
-		}
 		store.AddSong(newSong)
 		got := store.GetSong(0)
 		assertEqual(t, got, newSong)
 	})
 
 	t.Run("can't add duplicate song", func(t *testing.T) {
-		newSong := songvote.Song{
-			Name:   "Creep",
-			Artist: "Radiohead",
-		}
 		store.AddSong(newSong)
 		store.AddSong(newSong)
 
