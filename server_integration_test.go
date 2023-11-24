740132f7cf0d59c487dcb5941bd100e2b8f70691
et-codes <ericthornton43@gmail.com>
2023-11-24 18:27:04 +0000

    Integration test

--- server_integration_test.go
+++ server_integration_test.go
@@ -1,19 +1,48 @@
 package songvote_test
 
 import (
+	"net/http"
+	"net/http/httptest"
 	"testing"
+
+	"github.com/et-codes/songvote"
+	"github.com/et-codes/songvote/internal/assert"
 )
 
 func TestGetAndAddSongs(t *testing.T) {
-	t.Skip("Skipping to work on other stuff")
-	// store := songvote.InMemorySongStore{}
-	// server := songvote.NewServer(&store)
-	// songName := "Enjoy the Silence"
+	store := songvote.NewInMemorySongStore()
+	server := songvote.NewServer(store)
+	songToAdd := songvote.Song{
+		Name:    "Mirror In The Bathroom",
+		Artist:  "Oingo Boingo",
+		LinkURL: "http://test.com",
+		Votes:   10,
+		Vetoed:  false,
+	}
 
-	// server.ServeHTTP(httptest.NewRecorder(), newPostSongRequest(songName))
+	t.Run("add song and get ID", func(t *testing.T) {
+		response := httptest.NewRecorder()
+		request := newPostSongRequest(songToAdd)
+		server.ServeHTTP(response, request)
 
-	// response := httptest.NewRecorder()
-	// server.ServeHTTP(response, newGetSongRequest(songName))
-	// assertStatus(t, response.Code, http.StatusOK)
-	// assertResponseBody(t, response.Body.String(), songName)
+		got := response.Body.String()
+		want := "1"
+
+		assert.Equal(t, response.Code, http.StatusAccepted)
+		assert.Equal(t, got, want)
+	})
+
+	t.Run("get song with ID", func(t *testing.T) {
+		response := httptest.NewRecorder()
+		request := newGetSongRequest(1)
+		server.ServeHTTP(response, request)
+
+		got := songvote.Song{}
+		_ = songvote.UnmarshalSong(response.Body, &got)
+		want := songToAdd
+		want.ID = 1
+
+		assert.Equal(t, response.Code, http.StatusOK)
+		assert.Equal(t, got, want)
+	})
 }
