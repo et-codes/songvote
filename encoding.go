2e69d97bedcd8844b367c9db2f92c9d13bddb576
et-codes <ericthornton43@gmail.com>
2023-11-27 17:27:16 +0000

    Refactoring

--- encoding.go
+++ encoding.go
@@ -1,7 +1,6 @@
 package songvote
 
 import (
-	"bytes"
 	"encoding/json"
 	"fmt"
 	"io"
@@ -9,20 +8,22 @@ import (
 
 // MarshalJSON returns JSON-encoded string of an object and an error if there is one.
 func MarshalJSON(obj any) (string, error) {
-	output := bytes.NewBuffer([]byte{})
-	err := json.NewEncoder(output).Encode(obj)
+	output, err := json.Marshal(obj)
 	if err != nil {
-		return "", fmt.Errorf("problem encoding to JSON: %w", err)
+		return "", fmt.Errorf("problem marshalling JSON: %w", err)
 	}
-	return output.String(), nil
+	return string(output), nil
 }
 
-// UnmarshalJSON decodes the JSON string into the references struct and returns
+// UnmarshalJSON decodes the JSON string into the referenced struct and returns
 // an error if there is one.
-func UnmarshalJSON[T any](input io.Reader, obj *T) error {
-	err := json.NewDecoder(input).Decode(obj)
+func UnmarshalJSON[T any](in io.Reader, obj *T) error {
+	data, err := io.ReadAll(in)
 	if err != nil {
-		return fmt.Errorf("problem decoding from JSON: %w", err)
+		return fmt.Errorf("problem reading input: %w", err)
+	}
+	if err := json.Unmarshal(data, obj); err != nil {
+		return fmt.Errorf("problem unmarshalling JSON: %w", err)
 	}
 	return nil
 }
