5acd37a7409a46c9f9c0eddd342f92be871ffe29
et-codes <ericthornton43@gmail.com>
2023-12-19 20:58:40 +0000

    Create Server

--- main.go
+++ main.go
@@ -1,25 +1,12 @@
 package main
 
-import (
-	"log"
-	"log/slog"
-	"net/http"
-	"text/template"
-
-	"github.com/gorilla/mux"
-)
+import "log"
 
 const (
 	port = ":5050"
 )
-//nolint
-var tpl = template.Must(template.ParseGlob("templates/*"))
 
 func main() {
-	router := mux.NewRouter()
-
-	router.HandleFunc("/", index)
-
-	slog.Info("Server listening...", "port", port)
-	log.Fatal(http.ListenAndServe(port, router))
+	server := NewServer(port)
+	log.Fatal(server.ListenAndServe())
 }
---
+++ server.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"log/slog"
+	"net/http"
+	"text/template"
+
+	"github.com/gorilla/mux"
+)
+
+type Server struct {
+	port string
+	tpl *template.Template
+}
+
+func NewServer(port string) *Server {
+	var tpl = template.Must(template.ParseGlob("templates/*"))
+
+	return &Server{
+		port: port,
+		tpl: tpl,
+	}
+}
+
+func (s *Server) ListenAndServe() error {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/", s.index)
+
+	slog.Info("Server listening...", "port", port)
+	err := http.ListenAndServe(port, router)
+
+	return err
+}
+
+func (s *Server) index(w http.ResponseWriter, r *http.Request) {
+	if err := s.tpl.ExecuteTemplate(w, "index.gohtml", nil); err != nil {
+		slog.Error(err.Error())
+	}
+}
