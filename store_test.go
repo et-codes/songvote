ccb2e78418a750e88eed9188679b1a7f216e4b40
et-codes <ericthornton43@gmail.com>
2023-12-19 22:28:13 +0000

    Refactor tests

--- store_test.go
+++ store_test.go
@@ -8,17 +8,21 @@ import (
 
 func TestCreateUser(t *testing.T) {
 	s, _ := NewStore(":memory:")
-	req := NewUserRequest{"John Doe", "password"}
-	id, err := s.CreateUser(req)
 
-	assert.NoError(t, err)
-	assert.Equal(t, id, int64(1))
-	t.Log(id)
+	t.Run("creates user and gets id", func(t *testing.T) {
+		req := NewUserRequest{"John Doe", "password"}
+		id, err := s.CreateUser(req)
+		assert.NoError(t, err)
+		assert.Equal(t, id, int64(1))
+		t.Log(id)
+	})
 
-	user, err := s.GetUser(id)
-	assert.NoError(t, err)
-	assert.Equal(t, "John Doe", user.Name)
-	assert.Equal(t, "password", user.Password)
-	assert.False(t, user.Inactive)
-	assert.Greater(t, user.Vetoes, 0)
+	t.Run("user contains correct info", func(t *testing.T) {
+		user, err := s.GetUser(1)
+		assert.NoError(t, err)
+		assert.Equal(t, "John Doe", user.Name)
+		assert.Equal(t, "password", user.Password)
+		assert.False(t, user.Inactive)
+		assert.Greater(t, user.Vetoes, 0)
+	})
 }
